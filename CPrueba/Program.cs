2399010f6bd8d55e75cc841678b156a433c04ee1
Miguel Consoro German <miguelconso@hotmail.es>
2018-08-06 15:42:57 -0400

    Add project files.

---
+++ CPrueba/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using PuppeteerSharp;
+
+namespace CPrueba
+{
+    class MainClass
+    {
+        public static async Task Main(string[] args)
+        {
+            Test tmpTest = new Test();
+            await tmpTest.InjectHTML();
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ CPrueba/Test.cs
@@ -0,0 +1,32 @@
+﻿using PuppeteerSharp;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CPrueba
+{
+    public class Test
+    {
+        public async Task InjectHTML()
+        {
+            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = false
+            });
+            var tmpFilePath = Path.Combine("C://Users//mconsoro//source//repos//CPrueba//CPrueba//Resources", "WebPage.html");
+            string tmpHtml = File.ReadAllText(tmpFilePath);
+
+            var page = await browser.NewPageAsync();
+               
+            await page.SetContentAsync(tmpHtml);
+
+            var result = await page.GetContentAsync();
+            await page.SetViewportAsync(new ViewPortOptions { Width = 910, Height = 2050 });
+
+            await page.ScreenshotAsync("C://Users//mconsoro//source//repos//CPrueba//CPrueba//Resources//ImageWeb.png");
+
+        }
+    }
+}
